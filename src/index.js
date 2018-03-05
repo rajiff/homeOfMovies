402ab45555f83defa441bfa3e24ba8f9dc86e6e0
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-03-05 12:22:28 +0530

    Added some more component and reused those components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import renderWelcome from './welcome';
 import feedback from './components/feedback';
+import movies from './components/movies';
 
 function render() {
-	return `
+  return `
 		<h2>Welcome to ES 6 JS based Single Page App</h2>
 		${renderWelcome()}
 		<div id='feedback' style='padding: 20px; background: #eeeeee;'>
+		</div>
+		<div id='movies'>
 		</div>
 	`;
 }
 
 document.getElementById('app').innerHTML = render();
+
 document.getElementById('feedback').appendChild(feedback());
+
+let movieColln = [
+  { title: 'Movie-1', description: 'Movie-ONE' },
+  { title: 'Movie-2', description: 'Movie-TWO' },
+  { title: 'Movie-3', description: 'Movie-THREE' }
+];
+document.getElementById('movies').appendChild(movies(movieColln));
